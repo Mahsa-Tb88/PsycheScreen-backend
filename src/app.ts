e84c247f542ac9fdf231f0e6406d190f912b4880
Mahsa <50694999+Mah-tab88@users.noreply.github.com>
2025-09-24 21:40:56 -0700

    connect to database

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import "./utils/globalConstants.js";
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors"
+import responseMiddleware from "./middlewares/responseMiddleware.js";
+import mongoose from "mongoose";
 
 
 const app= express();
@@ -11,6 +13,16 @@ const app= express();
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({credentials:true,origin:true}))
+app.use(responseMiddleware)
+
+
+try{
+    await mongoose.connect(MONGO_URI);
+    console.log("connected to database successfyllu!")
+}catch(e:any){
+    console.log(e.message);
+    process.exit()
+}
 
 
 app.listen(PORT,()=>{
